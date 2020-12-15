c768fc8e672a0c3e95594471c378b694d52fbd04
Arielle Ross-Levy <ariellerosslevy@Arielles-MacBook-Air.local>
2020-12-15 12:41:50 -0700

    did assignment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
+require 'pry'
 def oxford_comma(array)
-
+new_string = ""
+    if array.length == 1
+    new_string = array.join
+ elsif array.length == 2
+    new_string = array.join(" and ")
+ elsif array.length > 2
+    new_string = array[0..-2].join(', ')
+    new_string << ', and '
+    new_string << array[-1].to_s
+  end
 end
+
+
